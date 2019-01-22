3b2b3cf2fbec7b9126723622e732b587fc682a57
Song <rn530263@dal.ca>
2019-01-21 20:34:50 -0400

    basic Android Studio clicking button application

--- app/src/main/java/com/example/acme/csci3130demo/MainActivity.java
+++ app/src/main/java/com/example/acme/csci3130demo/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.acme.csci3130demo;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +13,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        final EditText et = findViewById(R.id.editText);
+        final TextView t = findViewById(R.id.textView2);
+        final Button b = findViewById(R.id.button);
+        b.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                String text = et.getText().toString();
+                t.setText(text);
+            }
+        });
+
     }
+
+
 }
